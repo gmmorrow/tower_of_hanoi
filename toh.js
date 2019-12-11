acc0c1e0fc5181961ab1365357fb86e55e8c0265
gabrielle@Gabbys-MacPro.com <gab_morrow@yahoo.com>
2019-12-11 15:14:54 -0500

    commit 100000

--- toh.js
+++ toh.js
@@ -1,75 +1,69 @@
+const disk4 = document.getElementById("d4");
+const disk3 = document.getElementById("d3");
+const disk2 = document.getElementById("d2");
+const disk1 = document.getElementById("d1");
+const tower1 = document.getElementById("tA");
+const tower2 = document.getElementById("tB");
+const tower3 = document.getElementById("tC");
+let currentL = "string";
 
+let compare = function(a, b) {
+  console.log(a.lastElementChild);
+  let styleA = window.getComputedStyle(a);
+  let styleB = window.getComputedStyle(b);
+  let widthA = styleA.getPropertyValue("width");
+  let widthB = styleB.getPropertyValue("width");
 
-const disk4 = document.getElementById("d4")
-const disk3 = document.getElementById("d3")
-const disk2 = document.getElementById("d2")
-const disk1 = document.getElementById("d1")
-const tower1 = document.getElementById("tA")
-const tower2 = document.getElementById("tB")
-const tower3 = document.getElementById("tC")
-let currentL = "string"
+  widthA = parseInt(widthA);
+  widthB = parseInt(widthB);
+  if (widthA > widthB) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-let compare = function(a,b){
-    console.log(a.lastElementChild)
-    let styleA = window.getComputedStyle(a)
-    let styleB = window.getComputedStyle(b)
-    let widthA = styleA.getPropertyValue("width")
-    let widthB = styleB.getPropertyValue("width")
-
-    widthA = parseInt(widthA)
-    widthB = parseInt(widthB)
-    if(widthA>widthB){
-        return true
+let move = function() {
+  //let target = event.currentTarget.lastElementChild
+  console.log("function start", currentL);
+  if (currentL == "string") {
+    let currentTower = event.currentTarget;
+    let lastDisk = currentTower.lastElementChild;
+    currentL = lastDisk;
+    currentL.classList.add("selected");
+    console.log(currentL, "3rd log");
+  } else {
+    if (event.currentTarget.lastElementChild) {
+      console.log(currentL, event.currentTarget.lastElementChild, "nth log");
+      if (compare(currentL, event.currentTarget.lastElementChild)) {
+        alert("invalid move");
+        currentL.classList.remove("selected");
+        currentL = "string";
+        console.log(currentL, event.currentTarget.lastElementChild, "4th log");
+      } else {
+        event.currentTarget.appendChild(currentL);
+        currentL.classList.remove("selected");
+        currentL = "string";
+        console.log(
+          logcurrentL,
+          event.currentTarget.lastElementChild,
+          "5th log"
+        );
+      }
+    } else {
+      event.currentTarget.appendChild(currentL);
+      currentL.classList.remove("selected");
+      currentL = "string";
+      console.log(currentL, event.currentTarget.lastElementChild, "5th log");
     }
-    else{
-        return false
-    }
-}
+  }
+};
 
-let move = function(){
-    //let target = event.currentTarget.lastElementChild
-    console.log("function start", currentL)
-    if(currentL == "string"){
-        let currentTower = event.currentTarget
-        let lastDisk = currentTower.lastElementChild
-        currentL = lastDisk
-        currentL.classList.add("selected") 
-        console.log(currentL, "3rd log")
-    }
-    else{
-        if(event.currentTarget.lastElementChild){ 
-            console.log (currentL, event.currentTarget.lastElementChild, "nth log")
-            if(compare(currentL, event.currentTarget.lastElementChild)){
-                alert("invalid move")
-                currentL.classList.remove("selected")
-                currentL = "string"
-                console.log(currentL, event.currentTarget.lastElementChild, "4th log")
-            }
-            else{
-                event.currentTarget.appendChild(currentL)
-                currentL.classList.remove("selected")
-                currentL = "string"
-                console.log(logcurrentL, event.currentTarget.lastElementChild, "5th log")
-            }
-        }
-        else{
-            event.currentTarget.appendChild(currentL)
-            currentL.classList.remove("selected")
-            currentL = "string"
-            console.log(currentL, event.currentTarget.lastElementChild, "5th log")
-        }
-        }
-    
-}
+tower1.onclick = move;
 
+tower2.onclick = move;
 
-tower1.onclick = move
-
-tower2.onclick = move
-
-tower3.onclick = move
-    
-
+tower3.onclick = move;
 
 /*
 1. Display Towers
